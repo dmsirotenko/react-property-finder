479675311b54c9bcc2dc78ec9d96237ef1982459
dmsirotenko <dmitriy.sirotencko@gmail.com>
2018-02-03 20:13:05 +0300

    SearchPage Component

--- App.js
+++ App.js
@@ -7,22 +7,10 @@
 
 import React, { Component } from 'react';
 import {
-  Platform,
   StyleSheet,
-  Text,
-  View,
   NavigatorIOS
 } from 'react-native';
-
-class SearchPage extends Component<{}> {
-  render() {
-    return (
-      <Text style={styles.description}>
-        Search for houses to buy!
-      </Text>
-    );
-  }
-}
+import SearchPage from './SearchPage';
 
 export default class App extends Component<{}> {
   render () {
@@ -38,12 +26,6 @@ export default class App extends Component<{}> {
 }
 
 const styles = StyleSheet.create({
-  description: {
-    fontSize: 18,
-    textAlign: 'center',
-    color: '#656565',
-    marginTop: 65
-  },
   container: {
     flex: 1
   }
