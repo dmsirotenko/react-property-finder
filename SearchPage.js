c389bda89110e117ce17c05038e84997a79d67c1
dmsirotenko <dmitriy.sirotencko@gmail.com>
2018-02-05 01:10:55 +0300

    TextInput & Component State

--- SearchPage.js
+++ SearchPage.js
@@ -12,6 +12,20 @@ import {
 } from 'react-native';
 
 export default class SearchPage extends Component<{}> {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      searchString: 'london'
+    };
+  }
+
+  onSearchTextChanged = (event) => {
+    console.log('_onSearchTextChanged');
+    this.setState({ searchString: event.nativeEvent.text });
+    console.log(`Current: ${this.state.searchString}, Next: ${event.nativeEvent.text}`);
+  }
+
   render () {
     return (
       <View style={styles.container}>
@@ -21,6 +35,20 @@ export default class SearchPage extends Component<{}> {
         <Text style={styles.description}>
           Search by place or postcode.
         </Text>
+        <View style={styles.flowRight}>
+          <TextInput 
+            style={styles.searchInput}
+            value={this.state.searchString}
+            onChange={this.onSearchTextChanged}
+            placeholder='Search via name or postcode'
+          />
+          <Button 
+            onPress={() => {}}
+            color='#48BBEC'
+            title='Go'
+          />
+        </View>
+        <Image source={require('./resources/house.png')} style={styles.image}/>
       </View>
     );
   }
@@ -34,8 +62,29 @@ const styles = StyleSheet.create({
     color: '#656565'
   },
   container: {
-    padding: 30,
+    padding: 20,
     marginTop: 65,
     alignItems: 'center'
+  },
+  flowRight: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    alignSelf: 'stretch'
+  },
+  searchInput: {
+    height: 36,
+    padding: 4,
+    marginRight: 5,
+    flexGrow: 1,
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: '#48BBEC',
+    borderRadius: 4,
+    color: '#48BBEC',
+  },
+  image: {
+    width: 217,
+    height: 138,
+    marginTop: 20
   }
 })
